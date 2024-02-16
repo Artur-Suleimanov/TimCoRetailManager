c6bd76816342208915d1cd61d69f5aedb7be68e9
Артур Сулейманов <artur.suleimanov1997@gmail.com>
2024-02-16 17:36:21 +0400

    Fix Log Off Bug
    Fixes the bug where the header wasnot being cleared on log off.

--- TRMDesktopUI.Library/Api/IAPIHelper.cs
+++ TRMDesktopUI.Library/Api/IAPIHelper.cs
@@ -8,6 +8,7 @@ namespace TRMDesktopUI.Library.Api
     {
         Task<AuthenticatedUser> Authenticate(string username, string password);
         Task GetLoggedInUserInfo(string token);
+        void LogOffUser();
         HttpClient ApiClient { get; }
     }
 }
