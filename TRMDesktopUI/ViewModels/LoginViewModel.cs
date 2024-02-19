1009040b53ec5cfd34c1fd7f34454daf5c211a4e
Артур Сулейманов <artur.suleimanov1997@gmail.com>
2024-02-19 23:55:19 +0400

    Upgraded WPF to .NET Core 6.0

--- TRMDesktopUI/ViewModels/LoginViewModel.cs
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -97,7 +97,7 @@ namespace TRMDesktopUI.ViewModels
 				// Capture more information aout the user
 				await _apiHelper.GetLoggedInUserInfo(result.Access_Token);
 
-				_events.PublishOnUIThread(new LogOnEvent());
+				await _events.PublishOnUIThreadAsync(new LogOnEvent());
 
             }
 			catch (Exception ex)
